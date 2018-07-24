2db6275bc6899edf30d221e05dabcff3ce4bc68e
Fernando Mendoza <goliat8903@gmail.com>
2018-07-23 20:36:04 -0500

    Initial commit

---
+++ app/config/constants.js
@@ -0,0 +1,8 @@
+export const FIREBASE_API_KEY = "AIzaSyDW_LNHfcY7aaMdPzrStOay6yRiVrP-xdw";
+export const FIREBASE_AUTH_DOMAIN = "react-quotes-57275.firebaseapp.com";
+export const FIREBASE_DATABASE_URL = "https://react-quotes-57275.firebaseio.com";
+export const FIREBASE_PROJECT_ID = "react-quotes-57275";
+export const FIREBASE_STORAGE_BUCKET = "react-quotes-57275.appspot.com";
+export const FIREBASE_MESSAGING_SENDER_ID = "741358466377";
+
+export const FACEBOOK_APP_ID = 228599147769668;
